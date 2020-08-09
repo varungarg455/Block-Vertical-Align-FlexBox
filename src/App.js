eed4a4efc8e96f88c8eaa1698bbd757e4cb50fb0
Varun Garg <varungarg455@gmail.com>
2020-08-09 20:33:45 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.scss";
+
+export default function App() {
+  return (
+    <div className="App">
+      <div className="parent">
+        <div className="child"></div>
+      </div>
+    </div>
+  );
+}
